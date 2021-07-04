48701b06fef1074a1b185197f44e99b6063ed8eb
unknown <sculac.aldina@gmail.com>
2021-07-04 16:13:44 +0200

    Chakra UI - dodana

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { ChakraProvider, ColorModeScript, extendTheme } from '@chakra-ui/react';
 
+const config = {
+  initialColorMode: 'light',
+  useSystemColoMode: false
+}
 
+const theme = extendTheme({
+  config,
+})
+
+//<ColorModeScript initialColorMode="light" /> 
 ReactDOM.render(
-  <App />,
+  <ChakraProvider>
+    <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+    <App />
+  </ChakraProvider>,
   document.getElementById('root')
 );
